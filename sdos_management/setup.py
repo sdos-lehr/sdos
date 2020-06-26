1b0c897cfcb64b1bd7a12a89be115bd2e1b45e29
markus <sdos.lehr@gmail.com>
2020-06-26 15:02:30 +0200

    Add .deb package generator

---
+++ sdos_management/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sdos',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/sdos-lehr/sdos',
+    license='MIT',
+    author='markus lehr',
+    author_email='sdos.lehr@gmail.com',
+    description='Reference implementation for sdos-management node '
+)
